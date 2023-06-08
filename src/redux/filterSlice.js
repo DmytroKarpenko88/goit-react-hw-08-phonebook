bf7e505b3bf3544db52dd6d86b20d3bfad9a67c1
Dmytro Karpenko <kardmitriy@gmail.com>
2023-06-08 09:01:11 +0300

    filterSlice

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -2,13 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const filterSlice = createSlice({
   name: 'filter',
-  initialState: {
-    filter: '',
-  },
+  // initialState: {
+  //   filter: '',
+  // },
+  initialState: '',
   reducers: {
     findContact: {
       reducer(state, action) {
-        state.filter = action.payload;
+        // state.filter = action.payload;
+        return state = action.payload;
       },
     },
   },
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,5 @@
 export const getContacts = state => state.contacts.contacts;
 // export const getFilter = state => state.contacts.filter;
-export const getFilter = state => state.filter.filter;
+// export const getFilter = state => state.filter.filter;
+export const getFilter = state => state.filter;
 export const getActive = state => state.isActive;
